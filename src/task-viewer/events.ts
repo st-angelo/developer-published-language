3740a4968cd344e11837c7de085fd7953b38fadb
AngeloStatescu <35111313+AngeloStatescu@users.noreply.github.com>
2022-06-08 00:59:58 +0300

    added task-viewer entities

---
+++ src/task-viewer/events.ts
@@ -0,0 +1,13 @@
+enum Events {
+  GetIssues = 'GetIssues',
+  Issue = 'Issue',
+  AddIssue = 'AddIssue',
+  DeleteIssue = 'DeleteIssue',
+  IssueExists = 'IssueExists',
+  IssueNotFound = 'IssueNotFound',
+  IssueDeleted = 'IssueDeleted',
+  IssueInvalid = 'IssueInvalid',
+  IssueIsCompleted = 'IssueIsCompleted',
+}
+
+export default Events;
